96cea7077c538f0e6c99c89a331b55d56e670a8b
Yanari Taeko <tae.ienari@gmail.com>
2026-04-13 11:39:56 +0900

    Add model and factory

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Contact;
+use App\Models\Category;
+
+class ContactFactory extends Factory
+{
+
+    protected $model = Contact::class;
+
+    public function definition()
+    {
+        return [
+            'category_id' => $this->faker->numberBetween(1, 5),
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'gender' => $this->faker->numberBetween(1, 3),
+            'email' => $this->faker->unique()->safeEmail(),
+            'tel' => $this->faker->phoneNumber(),
+            'address' => $this->faker->address(),
+            'building' => $this->faker->optional()->secondaryAddress(),
+            'detail' => $this->faker->realText(50),
+
+        ];
+    }
+}
